107977e5a4a1c07c8fad4dbacbe71d2a665bcfec
Marcos Paulo <marcospsilva222@hotmail.com>
2021-01-08 16:37:10 -0300

    reinitialized files

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import "reflect-metadata";
+
+import express from 'express';
+
+import routes from './routes';
+
+import './database/connect';
+
+
+const app = express();
+
+app.use(express.json()); // sempre vem antes das rotas
+app.use(routes);
+
+app.listen(3000, ()=> console.log('Server started at http://localhost:3000'));
+
